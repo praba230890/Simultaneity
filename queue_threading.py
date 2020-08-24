42d84b63c02dc047e2e185ff952d7a0d2d373ba8
Prabakaran Santhanam <praba230890@gmail.com>
2020-08-25 03:04:34 +0530

    using queue instead of lock!

--- queue_threading.py
+++ queue_threading.py
@@ -1,15 +1,16 @@
 import threading, queue
 import time
 
-q = queue.Queue(maxsize=1)
-q.put(0)
+
+lock = threading.Lock()
+q = queue.Queue()
 
 def task(a, b, i):
-    d = q.get()
+    d = 0
     for v in range(100000):
         d += 1
-    print("The result of addition is: %s for thread %s" % (a + b, i))
     q.put(d)
+    print("The result of addition is: %s for thread %s" % (a + b, i))
     time.sleep(1)
 
 if __name__ == "__main__":
@@ -18,8 +19,11 @@ if __name__ == "__main__":
         add_task = threading.Thread(target=task, args=(5,5,i,))
         add_task.start()
         tasks.append(add_task)
+    
     for task in tasks:
         task.join()
-
-print(q.get())
-q.task_done()
+    
+    out = 0
+    while not q.empty():
+        out += q.get()
+    print(out)
